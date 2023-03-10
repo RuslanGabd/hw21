71addbae6dfbd856dc06c9ef219865e7897bb13b
heckfy86 <gmail08@yandex.ru>
2023-03-10 14:23:23 +0100

    commit message

---
+++ src/test/java/android/TestBase.java
@@ -0,0 +1,36 @@
+package android;
+
+import com.codeborne.selenide.Configuration;
+import com.codeborne.selenide.Selenide;
+import com.codeborne.selenide.logevents.SelenideLogger;
+import io.qameta.allure.selenide.AllureSelenide;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeAll;
+import drivers.BrowserStackMobileDriver;
+import org.junit.jupiter.api.BeforeEach;
+
+import static com.codeborne.selenide.Selenide.closeWebDriver;
+import static com.codeborne.selenide.Selenide.open;
+
+
+public class TestBase {
+
+    @BeforeAll
+    static void BeforeAll() {
+        Configuration.browser = BrowserStackMobileDriver.class.getName();
+        Configuration.browserSize = null;
+    }
+
+    @BeforeEach
+    void addListener() {
+        SelenideLogger.addListener("AllureSelenide", new AllureSelenide());
+        open();
+    }
+
+    @AfterEach
+    void AfterEach() {
+        closeWebDriver();
+    }
+
+
+}
---
+++ src/test/java/drivers/BrowserStackMobileDriver.java
@@ -0,0 +1,49 @@
+package drivers;
+
+import com.codeborne.selenide.WebDriverProvider;
+import io.appium.java_client.android.AndroidDriver;
+import org.openqa.selenium.Capabilities;
+import org.openqa.selenium.MutableCapabilities;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.remote.DesiredCapabilities;
+import org.openqa.selenium.remote.RemoteWebDriver;
+
+import javax.annotation.Nonnull;
+import java.net.MalformedURLException;
+import java.net.URL;
+
+public class BrowserStackMobileDriver implements WebDriverProvider {
+    @Nonnull
+    @Override
+    public WebDriver createDriver(@Nonnull Capabilities capabilities) {
+        //    DesiredCapabilities caps = new DesiredCapabilities();
+
+        MutableCapabilities mutableCapabilities = new MutableCapabilities();
+        mutableCapabilities.merge(capabilities);
+
+        // Set your access credentials
+        mutableCapabilities.setCapability("browserstack.user", "bsuser_CMlQsT");
+        mutableCapabilities.setCapability("browserstack.key", "qnfSRzumeiqN9knqvUrW");
+
+        // Set URL of the application under test
+        mutableCapabilities.setCapability("app", "bs://c700ce60cf13ae8ed97705a55b8e022f13c5827c");
+
+        // Specify device and os_version for testing
+        mutableCapabilities.setCapability("device", "Google Pixel 3");
+        mutableCapabilities.setCapability("os_version", "9.0");
+
+        // Set other BrowserStack  mutableCapabilities
+        mutableCapabilities.setCapability("project", "First Java Project");
+        mutableCapabilities.setCapability("build", "browserstack-build-1");
+        mutableCapabilities.setCapability("name", "first_test");
+
+        try {
+            return new RemoteWebDriver(
+                    new URL("http://hub.browserstack.com/wd/hub"), mutableCapabilities);
+        } catch (MalformedURLException e) {
+            throw new RuntimeException(e);
+        }
+
+    }
+
+}
